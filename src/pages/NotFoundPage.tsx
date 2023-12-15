7c169270ec1b06b8b5ae3ca88db0d9cf81d3fecc
fdm1try <d.a.fedorov90@gmail.com>
2023-12-15 12:24:19 +0300

    update 404 page

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,5 +1,27 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+
 export const NotFoundPage = () => {
   return (
-    <div>NotFoundPage</div>
+    <>
+      <Header />
+      <main className='container'>
+        <Row>
+          <Col>
+            <Banner />
+            <section className='top-sales'>
+              <h2 className='text-center'>Страница не найдена</h2>
+              <p>
+                Извините, такая страница не найдена!
+              </p>
+          </section>
+          </Col>
+        </Row>
+      </main>
+      <Footer />
+    </>
   )
 }
