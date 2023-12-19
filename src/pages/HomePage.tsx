2d7bf33e7704ae0b6ffc2ca64e1f4b31679f9300
fdm1try <d.a.fedorov90@gmail.com>
2023-12-19 16:50:43 +0300

    add HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,25 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { TopSalesWidget } from '../components/TopSalesWidget';
+import { CatalogWidget } from '../components/CatalogWidget';
+
 export const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <>
+      <Header />
+      <main className='container'>
+        <Row>
+          <Col>
+            <Banner />
+            <TopSalesWidget />
+            <CatalogWidget />
+          </Col>
+        </Row>
+      </main>
+      <Footer />
+    </>
   )
 }
