bbf310d6b6708c55b9155f8f6d0b52d7145a0ff9
fdm1try <d.a.fedorov90@gmail.com>
2023-12-19 17:00:09 +0300

    add CatalogPage

--- src/pages/CatalogPage.tsx
+++ src/pages/CatalogPage.tsx
@@ -1,5 +1,34 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { CatalogWidget } from '../components/CatalogWidget';
+import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+
 export const CatalogPage = () => {
+  const location = useLocation();
+  const catalogWidgetRef = React.createRef<HTMLElement>();
+
+  useEffect(() => {
+    if (location.state?.searchQuery) {
+      setTimeout(() => catalogWidgetRef.current?.scrollIntoView(), 200);
+    }
+  }, [location.state]);  
+
   return (
-    <div>CatalogPage</div>
+    <>
+      <Header />
+      <main className='container'>
+        <Row>
+          <Col>
+            <Banner />
+            <CatalogWidget ref={catalogWidgetRef} withSearch={true} searchQuery={location.state?.searchQuery || undefined} />
+          </Col>
+        </Row>
+      </main>
+      <Footer />
+    </>
   )
 }
