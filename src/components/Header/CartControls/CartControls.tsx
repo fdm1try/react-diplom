28097ff6eaa33a061f795611facc7d9802c7b6d5
fdm1try <d.a.fedorov90@gmail.com>
2023-12-23 18:25:02 +0300

    update header cart controls

--- src/components/Header/CartControls/CartControls.tsx
+++ src/components/Header/CartControls/CartControls.tsx
@@ -1,8 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../../redux/store"
+
 export const CartControls = () => {
+  const itemsCount = useAppSelector(state => state.cart.items.length);
+
+  const navigate = useNavigate();
+
+  const handleClick = () => navigate('/cart.html');
+
   return (    
-    <div className='header-controls-pic header-controls-cart'>
-      {/* Do programmatic navigation on click to /cart.html */}
-      <div className='header-controls-cart-full'>1</div>
+    <div onClick={handleClick} className='header-controls-pic header-controls-cart'>
+      {itemsCount > 0 && (
+        <div className='header-controls-cart-full'>{itemsCount}</div>
+      )}
       <div className='header-controls-cart-menu'></div>
     </div>    
   )
