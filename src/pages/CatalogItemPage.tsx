50eb8b58880da8ffdcbe1c67adbad88cce704450
fdm1try <d.a.fedorov90@gmail.com>
2023-12-23 18:22:17 +0300

    add CatalogItem page

--- src/pages/CatalogItemPage.tsx
+++ src/pages/CatalogItemPage.tsx
@@ -1,5 +1,29 @@
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Footer } from '../components/Footer';
+import { Header } from '../components/Header';
+import { Banner } from '../components/Banner';
+import { useParams } from 'react-router-dom';
+import { useItemQuery } from '../redux/api';
+import { CatalogItem, TCatalogItemDetails } from '../components/Catalog/CatalogItem';
+
 export const CatalogItemPage = () => {
+  const { id } = useParams();
+  const { data, error, isLoading, isFetching } = useItemQuery<TCatalogItemDetails>(Number(id));
+
   return (
-    <div>CatalogItemPage</div>
+    <>
+      <Header />
+      <main className='container'>
+        <Row>
+          <Col>
+            <Banner />
+            { error && <span className='error-message'>{`${error}`}</span>}
+            { data && <CatalogItem view={'detailed'} item={data} /> }
+          </Col>
+        </Row>
+      </main>
+      <Footer />
+    </>
   )
 }
