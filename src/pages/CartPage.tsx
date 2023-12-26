1b435b670601d0e47205b4f645c0b812ca9dd8a8
fdm1try <d.a.fedorov90@gmail.com>
2023-12-26 18:03:42 +0300

    add Cart page

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,33 +1,70 @@
-import { Row, Col } from 'react-bootstrap';
-import { Header } from '../components/Header';
-import { Banner } from '../components/Banner';
+/* eslint-disable react-hooks/exhaustive-deps */
+import { useAppSelector, useAppDispatch } from '../redux/store';
 import { Cart } from '../components/Cart';
-import { Footer } from '../components/Footer';
-import { useAppSelector } from '../redux/store';
-import { Order } from '../components/Order';
+import { Order, TOrder } from '../components/Order';
+import { WithBanner } from './templates';
+import { useCreateOrderQuery, TRequestOrderParams } from '../redux/api'
+import { skipToken } from '@reduxjs/toolkit/query';
+import { useEffect, useState } from 'react';
+import { Loader } from '../components/Loader';
+import { Error } from '../components/Error';
+import { Modal } from '../components/Modal';
+import { resetCart } from '../redux/slices/CartSlice';
 
 export const CartPage = () => {
-  const itemCount = useAppSelector(state => state.cart.items.length);
+  const dispatch = useAppDispatch();
+  const items = useAppSelector((state) => state.cart.items);
+  const [orderData, setOrderData] = useState<TRequestOrderParams|typeof skipToken>(skipToken);
+  const { isSuccess, isLoading, isFetching, error, refetch } = useCreateOrderQuery(orderData);
+  const [createOrderError, setCreateOrderError] = useState<string>();
+
+  const handleOrderSubmit = (owner: TOrder) => {
+    setOrderData({owner, items});
+  }
+
+  useEffect(() => {
+    setCreateOrderError(error ? 'Не удалось создать заказ.' : undefined);
+  }, [error])
+
+  useEffect(() => {
+    if (!isSuccess) return;
+    dispatch(resetCart());
+  }, [isSuccess])
+
+  const errorDetails = () => {
+    if (!error || !('status' in error) || !error.data) return;
+    return `HTTP code ${error.status}: ${JSON.stringify(error.data)}`;
+  }
+
+  const handleErrorMessageClose = () => {
+    setOrderData(skipToken);
+  }
+
+  const handleSuccessModalClose = () => {
+    setOrderData(skipToken);
+  }
 
   return (
-    <>
-      <Header />
-      <main className='container'>
-        <Row>
-          <Col>
-            <Banner />
-            { itemCount === 0 && (
-              <>
-                <br />
-                <p className='text-center'>Корзина пуста.</p>
-              </>
-            )}
-            { itemCount > 0 && <Cart />  }
-            { itemCount > 0 && <Order /> }
-          </Col>
-        </Row>
-      </main>
-      <Footer />
-    </>
+    <WithBanner>
+      { isSuccess && (
+        <Modal title='Заказ успешно создан.' onHide={handleSuccessModalClose}>
+          Спасибо за покупку! Наш менеджер свяжется с вами в ближайшее время для уточнения деталей.
+        </Modal>
+      )}
+      { !(isLoading || isFetching) && !isSuccess && error && createOrderError && (
+        <Error message={createOrderError} details={errorDetails()} onRetry={refetch} onClose={handleErrorMessageClose} />
+      )}
+      { (isLoading || isFetching) && (
+        <Loader fullscreen />
+      )}
+      { items.length === 0 && (
+        <>
+          <br />
+          <p className='text-center'>Корзина пуста.</p>
+        </>
+      )}
+      { items.length > 0 && <Cart />  }
+      { !(isLoading || isFetching) && !error && items.length > 0 && <Order onSubmit={handleOrderSubmit}/> }
+    </WithBanner>
   )
 }
