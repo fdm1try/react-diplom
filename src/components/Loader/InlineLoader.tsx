390fb9fc1ff996d45d68d8a4ec047ebce8c1204f
fdm1try <d.a.fedorov90@gmail.com>
2023-12-26 18:03:16 +0300

    add Loader component

---
+++ src/components/Loader/InlineLoader.tsx
@@ -0,0 +1,9 @@
+export const InlineLoader = () => {
+  return (
+    <div className='loader-inline'>
+      <span></span>
+      <span></span>
+      <span></span>
+    </div>
+  )
+}
---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,23 @@
+import spinnerImage from '../../assets/img/loader.png';
+import { InlineLoader } from './InlineLoader';
+import './Loader.css';
+
+export interface ILoader {
+  fullscreen?: boolean;
+  inline?: boolean;
+}
+
+export const Loader: React.FC<ILoader> = (props) => {
+  
+  if (props.inline) return <InlineLoader />
+
+  const className = () => `loader ${props.fullscreen ? 'loader-fullscreen' : ''}`;
+
+  return (
+    <div className={className()}>
+      <div className='loader-spinner'>
+        <img className='loader-spinner-img' src={spinnerImage} />
+      </div>
+    </div>
+  )
+}
