1b276ebcb0a0adec6fe3bfa61d97c2f1568c0e88
fdm1try <d.a.fedorov90@gmail.com>
2023-12-26 18:08:08 +0300

    update CatalogItem page: add loader and error handling

--- src/pages/CatalogItemPage.tsx
+++ src/pages/CatalogItemPage.tsx
@@ -1,29 +1,32 @@
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import { Footer } from '../components/Footer';
-import { Header } from '../components/Header';
-import { Banner } from '../components/Banner';
 import { useParams } from 'react-router-dom';
 import { useItemQuery } from '../redux/api';
-import { CatalogItem, TCatalogItemDetails } from '../components/Catalog/CatalogItem';
+import { CatalogItem } from '../components/Catalog/CatalogItem';
+import { Error } from '../components/Error';
+import { Loader } from '../components/Loader';
+import { WithBanner } from './templates';
 
 export const CatalogItemPage = () => {
   const { id } = useParams();
-  const { data, error, isLoading, isFetching } = useItemQuery<TCatalogItemDetails>(Number(id));
+  const { data, error, isLoading, isFetching, refetch } = useItemQuery(Number(id));
+
+  const errorDetails = () => {
+    if (!error || !('status' in error) || !error.data) return;
+    return `HTTP code ${error.status}: ${JSON.stringify(error.data)}`;
+  }
 
   return (
-    <>
-      <Header />
-      <main className='container'>
-        <Row>
-          <Col>
-            <Banner />
-            { error && <span className='error-message'>{`${error}`}</span>}
-            { data && <CatalogItem view={'detailed'} item={data} /> }
-          </Col>
-        </Row>
-      </main>
-      <Footer />
-    </>
+    <WithBanner>
+      { (isLoading || isFetching) && (
+        <>
+          <br />
+          <Loader/> 
+          <br />
+        </>
+      )}
+      { !(isFetching || isLoading) && error && (
+        <Error message='Не удалось получить данные с сервера' details={errorDetails()} onRetry={refetch} />
+      )}
+      { !(isLoading || isFetching) && !error && data && <CatalogItem view={'detailed'} item={data} /> }
+    </WithBanner>
   )
 }
