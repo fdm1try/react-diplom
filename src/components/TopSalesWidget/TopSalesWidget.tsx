fb3798ed84bb5ec5e86291eba85890d3dd5ea0b9
fdm1try <d.a.fedorov90@gmail.com>
2023-12-26 18:04:46 +0300

    update TopSalesWidget component: add loader and error handling

--- src/components/TopSalesWidget/TopSalesWidget.tsx
+++ src/components/TopSalesWidget/TopSalesWidget.tsx
@@ -1,15 +1,26 @@
+import { Error } from '../Error';
 import { useTopSalesQuery } from '../../redux/api';
 import { CatalogList } from '../Catalog/CatalogList';
+import { Loader } from '../Loader';
+import { TCatalogItem } from '../Catalog/CatalogItem';
 
 export const TopSalesWidget = () => {
-  const { data, isLoading, isFetching } = useTopSalesQuery(null);
+  const { data, isLoading, isFetching, refetch, error } = useTopSalesQuery(null);
+  const errorMessage = 'Не удалось получить данные с сервера.';
 
-  if (!data) return <></>;
+  const errorDetails = () => {
+    if (!error || !('status' in error) || !error.data) return;
+    return `HTTP code ${error.status}: ${JSON.stringify(error.data)}`;
+  }
 
   return (
     <section className='top-sales'>
       <h2 className='text-center'>Хиты продаж!</h2>
-      <CatalogList view='card' items={data} />
+      { (isLoading || isFetching) && <Loader /> }
+      { !(isLoading || isFetching || error) && <CatalogList view='card' items={data as Array<TCatalogItem>} /> }
+      { !(isLoading || isFetching) &&error && (
+        <Error variant='inline' details={errorDetails()} message={errorMessage} onRetry={refetch} />
+      ) }
     </section>
   )
 }
