152ca863145eb1670900433cadd1497cc5cadedf
fdm1try <d.a.fedorov90@gmail.com>
2024-01-13 15:42:34 +0300

    update vite config: configure vite-plugin-pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import pluginRewriteAll from 'vite-plugin-rewrite-all';
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), pluginRewriteAll()],
+  plugins: [
+    react(),
+    pluginRewriteAll(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /loader\.png|banner\.jpg|header-logo\.png/i,
+            handler: 'CacheFirst',
+          }
+        ]
+      }
+    })],  
 })
