c08e5a787bfdfbe13096e106cc86a03704c07ec9
fdm1try <d.a.fedorov90@gmail.com>
2024-01-13 15:46:27 +0300

    update useCatalogFilter hook

--- src/hooks/useCatalogFilter.ts
+++ src/hooks/useCatalogFilter.ts
@@ -1,6 +1,6 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, useRef } from 'react';
-import { useItemsQuery, IRequestItemList } from '../redux/api';
+import { useItemsMutation } from '../redux/api';
 import { TCatalogItem } from '../components/Catalog/CatalogItem';
 
 const CATALOG_ITEMS_PER_PAGE = 6;
@@ -10,42 +10,57 @@ export interface IProps {
   categoryId?: number;
 }
 
+type TItemsState = {
+  items: Array<TCatalogItem>;
+  hasMoreItems: boolean;
+}
+
+const initialItemsState = { items: [], hasMoreItems: true };
+
 export const useCatalogFilter = (props: IProps) => {
   const prevProps = useRef<IProps>({...props});
+  const [itemsState, setItemsState] = useState<TItemsState>(initialItemsState);
+  const [fetchItems, { data, isLoading, error }] = useItemsMutation();
+  const request = useRef<{abort: () => void}>();
 
   const isPropsChanged = () => {
     const prev: IProps = prevProps.current;
-    if (!prev || prev.searchQuery !== props.searchQuery || prev.categoryId !== props.categoryId) return true;
-    return false;
+    return !prev || prev.searchQuery !== props.searchQuery || prev.categoryId !== props.categoryId;
   }
 
-  const [queryParams, setQueryParams] = useState<IRequestItemList>({q: props.searchQuery, categoryId: props.categoryId});
-  const [items, setItems] = useState<Array<TCatalogItem>>([]);
-  const [hasMoreItems, setHasMoreItems] = useState<boolean>(true);
-  const { data, isLoading, isFetching, error, refetch } = useItemsQuery(queryParams);
+  const fetchLatest = (offset: number = itemsState.items.length) => {
+    if (isLoading) request.current?.abort();
+    request.current = fetchItems({ q: props.searchQuery, categoryId: props.categoryId, offset });
+  }
+
+  useEffect(() => {
+    if (!isLoading && !itemsState.items.length) fetchLatest(0);
+    return () => {
+      request.current?.abort();
+      setItemsState(initialItemsState);
+    }
+  }, []);
 
   useEffect(() => {
     if (!isPropsChanged()) return;
     prevProps.current = props;
-    setItems([]);
-    setHasMoreItems(true);
-    setQueryParams({ q: props.searchQuery, offset: 0, categoryId: props.categoryId});
-  }, [props]);
+    setItemsState(initialItemsState);
+    fetchLatest(0);
+  }, [props]);  
 
-  useEffect(() => {
-    setItems([]);
-  }, []);
+  
 
   useEffect(() => {
     if (!data) return;
-    if (data.length < CATALOG_ITEMS_PER_PAGE) setHasMoreItems(false);
-    setItems((state) => [...state, ...data]);
+    const ids = itemsState.items.map((item) => item.id);
+    const newItems = data.filter((item) => !ids.includes(item.id))
+    setItemsState((state) => ({ items: [...state.items, ...newItems], hasMoreItems: data.length >= CATALOG_ITEMS_PER_PAGE}));
   }, [data])
 
   const loadMoreItems = () => {
-    if (!hasMoreItems) return;
-    setQueryParams((state) => ({...state, offset: (state.offset || 0) + CATALOG_ITEMS_PER_PAGE}));
+    if (!itemsState.hasMoreItems) return;
+    fetchLatest(itemsState.items.length + CATALOG_ITEMS_PER_PAGE);
   }
 
-  return { items, isLoading: isLoading || isFetching, error, hasMoreItems, loadMoreItems, refetch};
+  return { items: itemsState.items, isLoading, error, hasMoreItems: itemsState.hasMoreItems, loadMoreItems, refetch: fetchLatest};
 }
